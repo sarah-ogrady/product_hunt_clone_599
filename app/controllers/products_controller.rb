cf9841f25fac8651569287262659a8690b592a60
Sarah OGrady <sarahfrancesogrady@gmail.com>
2021-06-11 10:21:12 +0100

    run unit and system tests on GHA

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,21 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :tagline)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def full_name
+    return "#{first_name.capitalize} #{last_name.capitalize}"
+  # there is already an attribute_reader in application record
+  end
 end
